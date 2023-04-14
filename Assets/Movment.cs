6a4891e4cb4e6b905f05a96a7d6b6f9e7552cf0f
Amit Davidson <sgtamit234@gmail.com>
2023-04-14 17:00:08 +0300

    Changes

--- Assets/Movment.cs
+++ Assets/Movment.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class Movment : MonoBehaviour
 {
     private GameObject goal;
-    public float speed = 0.01f;
+    public float speed = 0.005f;
     void Start(){
         goal = GameObject.FindGameObjectWithTag("Main");
     }
   
     void Update()
-    {   
-        Vector3 direction = goal.transform.position - transform.position;
+    {
+        Vector3 realGoal = new Vector3(goal.transform.position.x, transform.position.y, goal.transform.position.z);
+        Vector3 direction = realGoal - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(direction, Vector3.up);
+        transform.rotation = rotation;
+        Debug.DrawRay(transform.position, direction, Color.green);
+        
         Vector3 pushVector = direction.normalized * speed;
         transform.Translate(pushVector, Space.World);                               
                 
