9abfeb3360fb0d9439af3b18497b339489e55054
Amit Davidson <sgtamit234@gmail.com>
2023-04-14 17:34:54 +0300

    a

--- Assets/Scripts/Movment.cs
+++ Assets/Scripts/Movment.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class Movment : MonoBehaviour
 {
+    
     private GameObject goal;
+    private bool _isChasing = true;
     public float speed = 0.005f;
     void Start(){
         goal = GameObject.FindGameObjectWithTag("Main");
@@ -14,12 +16,24 @@ public class Movment : MonoBehaviour
     {
         Vector3 realGoal = new Vector3(goal.transform.position.x, transform.position.y, goal.transform.position.z);
         Vector3 direction = realGoal - transform.position;
-        Quaternion rotation = Quaternion.LookRotation(direction, Vector3.up);
-        transform.rotation = rotation;
+        if (!_isChasing)
+        {
+            direction *= -1;
+        }
+        
         Debug.DrawRay(transform.position, direction, Color.green);
         
         Vector3 pushVector = direction.normalized * speed;
-        transform.Translate(pushVector, Space.World);                               
-                
+        transform.Translate(pushVector, Space.World);
+        
+        Quaternion rotation = Quaternion.LookRotation(direction, Vector3.up);
+        transform.rotation = rotation;
+
+
+        if (direction.magnitude <= Constants.CatchDistance)
+        {
+            direction *= -1;
+            _isChasing = !_isChasing;
+        }
     }
 }
