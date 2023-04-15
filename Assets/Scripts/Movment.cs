6de7827ed68214aa09cc01785d4ec3c2c5f6a3a8
Amit Davidson <sgtamit234@gmail.com>
2023-04-15 13:18:13 +0300

    Fix indent

--- Assets/Scripts/Movment.cs
+++ Assets/Scripts/Movment.cs
@@ -4,43 +4,44 @@ using UnityEngine;
 
 public class Movment : MonoBehaviour
 {
-    
     private GameObject goal;
     private bool _isChased = false;
     public float speed = 0.005f;
-    void Start(){
+
+    void Start()
+    {
         goal = GameObject.FindGameObjectWithTag("Main");
     }
 
     Vector3 GetMovement()
     {
-
         Vector3 goalVec = new Vector3(goal.transform.position.x, transform.position.y, goal.transform.position.z);
         Vector3 path = goalVec - transform.position;
         Debug.DrawRay(transform.position, path, Color.green);
-        
+
         Vector3 pushVector = path.normalized * speed;
         if (_isChased)
         {
             pushVector *= -1;
         }
+
         return pushVector;
     }
+
     void Update()
     {
         Vector3 goalVec = GetMovement();
         transform.Translate(goalVec, Space.World);
-        
+
         Quaternion rotation = Quaternion.LookRotation(goalVec, Vector3.up);
-        transform.rotation = rotation; 
+        transform.rotation = rotation;
     }
-    
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag.Equals("Main"))
         {
-            _isChased = true;    
+            _isChased = true;
         }
     }
-
 }
