1e11cc8e82799a347fdf2a9c135ebfaf633a103e
tomer <tomer.keinan.mail.huji.ac.il>
2023-04-15 16:55:56 +0300

    a

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -1,4 +1,8 @@
 public class Constants
 {
-    public const int CatchDistance = 3;
+    public const int maxStamina = 100;
+    public const float walkingSpeed = 2f;
+    public const float sprintSpeed = 4f;
+    public const double reduceStamina = 0.5;
+    public const double increasedStamina = 0.2;
 }
--- Assets/Scripts/Movment.cs
+++ Assets/Scripts/Movment.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -5,12 +6,14 @@ public class Movment : MonoBehaviour
 {
     [SerializeField] GameObject _goal;
     private bool _isChased = false;
-    private float speed = 0.01f;
+    private double currentStamina;
     private NavMeshAgent _agent;
 
     void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
+        _agent.speed = Constants.walkingSpeed;
+        currentStamina = Constants.maxStamina;
     }
 
     Vector3 GetMovement()
@@ -31,17 +34,36 @@ public class Movment : MonoBehaviour
 
     void Update()
     {
-        Vector3 testvec = new Vector3(0, 0, 0);
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            Debug.Log("Left Shift key is pressed!");
-            Debug.DrawLine(transform.position, testvec, Color.red);
-        }
+        Debug.Log(_agent.speed);
+        Debug.Log(currentStamina);
+        Debug.Log("----------");
+        CheckRunning();
         Vector3 goalVec = GetMovement();
         _agent.SetDestination(goalVec);
 
         Quaternion rotation = Quaternion.LookRotation(goalVec, Vector3.up);
         transform.rotation = rotation;
+    }
+
+    void CheckRunning()
+    {
+        double reducedStamina = currentStamina - Constants.reduceStamina;
+        double incresedStamina = currentStamina + Constants.increasedStamina;
+        if (Input.GetKey(KeyCode.LeftShift) && reducedStamina >= 0)
+        {
+            _agent.speed = Constants.sprintSpeed;
+            currentStamina = reducedStamina;
+        }
+        else
+        {
+            currentStamina = incresedStamina;
+            _agent.speed = Constants.walkingSpeed;
+            if(currentStamina > Constants.maxStamina)
+            {
+                currentStamina = Constants.maxStamina;
+            }
+        }
+
     }
 
     private void OnTriggerEnter(Collider other)
