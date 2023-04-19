f589d6821caa771180d9ef47b54c3b3506f1b89f
Amit Davidson <sgtamit234@gmail.com>
2023-04-19 17:42:56 +0300

    Changes

--- Assets/Scripts/MovementCamera.cs
+++ Assets/Scripts/MovementCamera.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovementPlayer : MonoBehaviour
+public class MovementCamera : MonoBehaviour
 {
     [SerializeField] private float speed;
 
@@ -11,22 +11,22 @@ public class MovementPlayer : MonoBehaviour
     {
         //returns the basic values, if it's 0 than it's not active.
         Vector3 p_Velocity = new Vector3();
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.UpArrow))
         {
             p_Velocity += new Vector3(0, 0, 1);
         }
 
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.DownArrow))
         {
             p_Velocity += new Vector3(0, 0, -1);
         }
 
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             p_Velocity += new Vector3(-1, 0, 0);
         }
 
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.RightArrow))
         {
             p_Velocity += new Vector3(1, 0, 0);
         }
