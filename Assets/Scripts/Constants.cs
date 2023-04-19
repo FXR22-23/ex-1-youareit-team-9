2490e6e1f47ff9fb4e1578107958562f0c51dca8
tomer <tomer.keinan.mail.huji.ac.il>
2023-04-19 16:46:01 +0300

    a

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -6,6 +6,6 @@ public class Constants
     public const double reduceStamina = 0.5;
     public const double increasedStamina = 0.2;
     public const int distanceToChase = 10;
-    public const int numOfObstcales = 30;
+    public const int numOfObstcales = 200;
     public const int spawnRadius = 70;
 }
