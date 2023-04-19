0fa00c04b135481873e3d12dd5eddc041fe34b94
tomer <tomer.keinan.mail.huji.ac.il>
2023-04-19 14:27:14 +0300

    a

--- Assets/Scripts/ObstacleGenerator.cs
+++ Assets/Scripts/ObstacleGenerator.cs
@@ -6,8 +6,6 @@ public class ObstacleGenerator : MonoBehaviour
 {
     public GameObject[] obstaclePrefabs = null; // Array of obstacle prefabs to use
     public Vector3 center = new Vector3(13.5f, -3f, -45.47f); // The center of the spawn area
-    public float spawnRadius = 40f; // The radius in which obstacles should be spawned
-    public int numObstacles = 10; // The number of obstacles to generate
     public LayerMask groundLayer; // The layer mask for the ground
     // Start is called before the first frame update
     void Start()
@@ -18,22 +16,17 @@ public class ObstacleGenerator : MonoBehaviour
         }
 
         // Generate random obstacles within spawn area
-        for (int i = 0; i < numObstacles; i++)
+        for (int i = 0; i < Constants.numOfObstcales; i++)
         {
-            // Generate random position within spawn area
-            Vector3 position = center + Random.insideUnitSphere * spawnRadius;
-
-            // Ensure that obstacle is placed on the ground
-            RaycastHit hit;
-            if (Physics.Raycast(position, Vector3.down, out hit, Mathf.Infinity, groundLayer))
-            {
-                position = hit.point;
-            }
-
-            // Instantiate random obstacle prefab at random position
             GameObject obstaclePrefab = obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)];
-            Instantiate(obstaclePrefab, position, Random.rotation);
 
+            // Generate random position within spawn area
+            Vector3 position = center + Random.insideUnitSphere * Constants.spawnRadius;
+
+            position.y = obstaclePrefab.transform.position.y;
+
+            Quaternion randomRotaion = Quaternion.Euler(0.0f, Random.Range(0.0f, 360.0f), 0.0f);
+            Instantiate(obstaclePrefab, position, randomRotaion);
         }
 
     }
